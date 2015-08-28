abbe7df91424b614362db8d4529130401e519212
Isac Arnekvist <isacar@kth.se>
2015-08-28 09:30:48 +0200

    argparse add now in tests

--- test.py
+++ test.py
@@ -25,9 +25,9 @@ class BasicTests(unittest.TestCase):
     def test_program(self):
         #os.system('cat ../korpus | tokenizer/tokenizer | grep telefonkontakt | wc -l >tmp.txt')
         #self.assertEqual( int(open('tmp.txt').read()), 8)
-        self.assertEqual(os.system('python3 reader.py tågpladder nostupid 1> /dev/null'), 0)
-        self.assertEqual(os.system('python3 reader.py för nostupid 1> /dev/null '), 0)
-        self.assertEqual(os.system('python3 reader.py regering nostupid 1> /dev/null'), 0)
+        self.assertEqual(os.system('python3 reader.py tågpladder --stupid 1> /dev/null'), 0)
+        self.assertEqual(os.system('python3 reader.py för 1> /dev/null '), 0)
+        self.assertEqual(os.system('python3 reader.py regering 1> /dev/null'), 0)
             
 
 if __name__=='__main__':
